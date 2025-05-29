e03e73fdd56ce669b4703ce451a80e32cb1f7025
algineer <120684984+algineer@users.noreply.github.com>
2025-05-29 03:46:21 -0400

    Query for latest chrome version and replace outdated in string

--- fetch-ua.js
+++ fetch-ua.js
@@ -1,6 +1,15 @@
 const puppeteer = require("puppeteer")
 const fs = require("fs")
 
+const getLatestChromeVersion = async () => {
+    const url = "https://versionhistory.googleapis.com/v1/chrome/platforms/win/channels/stable/versions"
+    const res = await fetch(url)
+    if (!res.ok) throw new Error(`Failed to fetch latest Chrome version: ${res.status}`)
+    const data = await res.json()
+    // The first element is the latest version object
+    return data.versions[0].version.split(".")[0]
+}
+
 ;(async () => {
     const browser = await puppeteer.launch({
         headless: "new", // or true if using older Puppeteer
@@ -10,9 +19,10 @@ const fs = require("fs")
     const page = await browser.newPage()
     const ua = await page.evaluate(() => navigator.userAgent)
 
+    ua = ua.replace(/HeadlessChrome\/\d+/, `Chrome/${getLatestChromeVersion()}`)
+
     // Write UA to JSON file
     fs.writeFileSync("ua.json", JSON.stringify({ userAgent: ua }, null, 2))
 
-    console.log("User Agent saved:", ua)
     await browser.close()
 })()
