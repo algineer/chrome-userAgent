88fab7d65b77a8cc69714a8ccb4f74e3abe6eb74
algineer <120684984+algineer@users.noreply.github.com>
2025-05-29 02:49:44 -0400

    Update fetch-ua.js

--- fetch-ua.js
+++ fetch-ua.js
@@ -2,10 +2,12 @@ const puppeteer = require("puppeteer")
 const fs = require("fs")
 
 ;(async () => {
-    const browser = await puppeteer.launch({ headless: true })
-    const page = await browser.newPage()
+    const browser = await puppeteer.launch({
+        headless: "new", // or true if using older Puppeteer
+        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    })
 
-    await page.goto("about:blank")
+    const page = await browser.newPage()
     const ua = await page.evaluate(() => navigator.userAgent)
 
     // Write UA to JSON file
